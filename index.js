7c614f249ed876eec0a587d22dc356379378cb0f
Diva Yurian DWika <divayuriandwika@gmail.com>
2020-06-02 21:59:33 +0700

    create database in mongo, create connection, environment and index in config with express, create .env

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { PORT, db } = require('./config');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.status(200).json({ messege: 'hello to monitor api ' });
+});
+
+if (db) {
+	app.listen(PORT, () => {
+		console.log(`this app listen to port ${PORT}`);
+	});
+}
